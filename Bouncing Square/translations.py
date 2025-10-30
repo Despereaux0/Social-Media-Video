5cba6e7ef80abef5c19dad2228ac738e7d8664ec
Vassant <145767722+Despereaux0@users.noreply.github.com>
2025-10-30 10:44:03 +0800

    Add files via upload

---
+++ Bouncing Square/translations.py
@@ -0,0 +1,154 @@
+contributors = "quasar098, TheCodingCrafter, PurpleJuiceBox, sled45, Times0, Spring-Forever-with-me, cangerjun, lucmsilva651, leo539, simpansoftware, slideglide, Guavvva"
+
+TRANSLATIONS = {
+    "english": {
+        "font": "poppins-regular.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "Play",
+        "config": "Config",
+        "contribute": "Contribute",
+        "open-songs-folder": "Open Songs Folder",
+        "quit": "Quit",
+
+        "back": "Back",
+        "title": "midi-playground",
+        "title-marquee": f"""Contributors: {contributors}. Just want to 
+watch the square and don't want to play the game? Turn on theatre mode in the config if that is the case. Interested in playing your 
+own song but don't know how? Check out docs/SONGS.md in the source code to learn how to add your own songs.""".replace("\n", ''),
+
+        "restart-required": "Restart Required"
+    },
+    "brazilian-pt": {
+        "font": "poppins-regular.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "Jogar",
+        "config": "Configurações",
+        "contribute": "Contribuir",
+        "open-songs-folder": "Abrir Pasta de Músicas",
+        "quit": "Sair",
+
+        "back": "Voltar",
+        "title": "midi-playground",
+        "title-marquee": f"""Contribuintes: {contributors}. 
+Quer apenas assistir ao quadrado e não quer jogar o jogo? Se for esse o caso, ative o Modo Teatro nas configurações. Tem interesse em colocar sua própria música, 
+mas não sabe como? Consulte docs/SONGS.md no código-fonte para saber como adicionar suas próprias músicas.""".replace("\n", ''),
+
+        "restart-required": "Reinicialização Necessária"
+    },
+    "spanish": {
+        "font": "poppins-regular.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "Jugar",
+        "config": "Configuraciones",
+        "contribute": "Contribuir",
+        "open-songs-folder": "Carpeta de Músicas",
+        "quit": "Salir",
+
+        "back": "Volver",
+        "title": "midi-playground",
+        "title-marquee": f"""Colaboradores: {contributors}. 
+¿Sólo quieres ver el cuadrado y no jugar el juego? Si es así, activa el Modo Teatro en los ajustes. 
+¿Te interesa añadir tu propia música pero no sabes cómo? Consulta docs/SONGS.md en el código fuente para saber cómo puedes poner tu propia música.""".replace("\n", ''),
+
+        "restart-required": "Reinicio Necesario"
+    },
+    "chinese": {
+        "font": "hanyisongjian.ttf",
+
+        "font-menu-shadow-offset": 24,
+        "font-menu-shadow-length-offset": -15,
+        "play": "开始游戏",
+        "config": "设置",
+        "contribute": "贡献",
+        "open-songs-folder": "打开歌曲文件",
+        "quit": "退出",
+
+        "back": "返回",
+        "title": "midi游乐场",
+        "title-marquee": f"""贡献者: {contributors}. 快来加入我们！
+想要看着方块自己动，不想玩游戏？
+如果是这样，请在设置中打开剧场模式。
+想要播放自己喜欢的歌曲，但不知道如何播放？
+查看文件夹中的docs/SONGS.md，了解如何添加自己喜欢的歌曲。""".replace("\n", ''),
+
+        "restart-required": "需要重新启动"
+    },
+    "french": {
+        "font" : "poppins-regular.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "Jouer",
+        "config": "Configuration",
+        "contribute": "Contribuer",
+        "open-songs-folder": "Dossier Chansons",
+        "quit": "Quitter",
+
+        "back": "Précédent",
+        "title": "midi-playground",
+        "title-marquee": f"""Contributeurs: {contributors}. Vous voulez simplement 
+regarder le carré et ne pas jouer au jeu ? Activez le mode théâtre dans la configuration si tel est le cas. Vous êtes intéressé à jouer votre propre chanson mais ne savez pas comment faire ? 
+Consultez docs/SONGS.md dans le code source pour apprendre comment ajouter vos propres chansons.""".replace("\n", ''),
+
+        "restart-required": "Redémarrage nécessaire"
+    },
+    "swedish": {
+        "font" : "poppins-regular.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "Spela",
+        "config": "Inställningar",
+        "contribute": "Bidra",
+        "open-songs-folder": "Öppna låt-mappen",
+        "quit": "Avsluta",
+
+        "back": "Tillbaka",
+        "title": "midi-playground",
+        "title-marquee": f"""Medhjälpare: {contributors}. Vill du bara se 
+kvadraten och inte vill spela? Sätt på teaterläge i konfigurationerna i så fall. Intresserad i att spela dina egna låtar men du vet inte hur? 
+Kolla docs/SONGS.md i källkoden för att lära dig hur du lägger till dina egna låtar.""".replace("\n", ''),
+
+        "restart-required": "Omstart krävs",
+    },
+    "turkish": {
+        "font": "poppins-regular.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "Oyna",
+        "config": "Yapılandırma",
+        "contribute": "Katkıda Bulun",
+        "open-songs-folder": "Şarkılar Klasörünü Aç",
+        "quit": "Çıkış",
+
+        "back": "Geri",
+        "title": "midi-oyunalanı",
+        "title-marquee": f"""Katkıda Bulunanlar: {contributors}. Sadece kareyi izliyor ve oyunu oynamak istemiyor musunuz? Durum böyleyse yapılandırmadan tiyatro modunu açın. Kendi şarkınızı oynamakla ilgilisiniz fakat nasıl yapılacağını bilmiyor musunuz? Kendi şarkılarınızı nasıl ekleyeceğinizi öğrenmek için kaynak koddaki docs/SONGS.md dosyasına göz atın.""".replace("\n", ''),
+
+        "restart-required": "Yeniden Başlatma Gerekiyor"
+    },
+    "russian": {
+        "font": "nunito-bold.ttf",
+
+        "font-menu-shadow-offset": 9,
+        "font-menu-shadow-length-offset": 0,
+        "play": "играть",
+        "config": "настройки",
+        "contribute": "репозиторий",
+        "open-songs-folder": "папка с песнями",
+        "quit": "выйти",
+
+        "back": "назад",
+        "title": "проигрыватель-миди",
+        "title-marquee": f"""Участники: {contributors}. Хочешь просто смотреть, а не играть? Включи режим "Theatre Mode". Как добавить песню? Перейди по docs/SONGS.md в репозитории чтобы узнать.""".replace("\n", ''),
+        
+        "restart-required": "требуется перезагрузка"
+    }
+}
